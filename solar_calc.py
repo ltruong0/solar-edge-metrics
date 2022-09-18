07b31a639269aa6d72efc0c3a150a4b7fcf0196d
ltruong0 <73973390+ltruong0@users.noreply.github.com>
2022-09-17 22:25:36 -0500

    Lastday (#1)
    * add last day, adjust return to floats
    
    * formatting

--- solar_calc.py
+++ solar_calc.py
@@ -6,11 +6,12 @@ from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 import pytz
 import logging
+import argparse
 
 
 def setup_logger():
     logger = logging.getLogger("solar")
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
     fh = logging.FileHandler("debug.log")
     sh = logging.StreamHandler()
     logger.addHandler(fh)
@@ -19,6 +20,17 @@ def setup_logger():
     fh.setFormatter(formatter)
     sh.setFormatter(formatter)
     return logger
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Post Metrics from Solar Edge')
+    parser.add_argument('-hr', '--hour', action='store_true',
+                        help='energy produced for last hour')
+    parser.add_argument('-d', '--day', action='store_true',
+                        help='energy produced for last day')
+    args = parser.parse_args()
+    return args
 
 
 async def get_power_details(siteId: int, apiKey: str, startTime: str, endTime: str):
@@ -42,6 +54,38 @@ async def get_energy_details(siteId: int, apiKey: str, startTime: str, endTime: 
     response = requests.request("GET", url, headers=headers)
 
     return response
+
+
+async def get_last_day_energy(siteId: int, apiKey: str):
+    # get energy for last day
+
+    # get current time stamp, rounded to day
+    current_timestamp = datetime.now()
+    logger.info(f"Current time is {current_timestamp}")
+
+    current_timestamp = current_timestamp.replace(
+        hour=0, second=0, minute=0, microsecond=0)
+
+    # subtract timestamp by 1 day
+    day_ago_timestamp = current_timestamp - timedelta(days=1)
+
+    logger.info(
+        f"Gathering Energy Data From {day_ago_timestamp} to {current_timestamp}")
+
+    # get energy details
+    response = await get_energy_details(siteId=siteId, apiKey=apiKey, startTime=str(day_ago_timestamp), endTime=str(current_timestamp), timeUnit="HOUR")
+    logger.debug(f"Response status code: {response.status_code}")
+    logger.debug(response.json())
+
+    total = 0
+    if response.status_code == 200:
+        for value in response.json()['energyDetails']['meters'][0]['values']:
+            total += value.get('value', 0)
+
+    # convert to float
+    total = float(total)
+
+    return total, day_ago_timestamp
 
 
 async def get_last_hour_energy(siteId: int, apiKey: str):
@@ -70,6 +114,8 @@ async def get_last_hour_energy(siteId: int, apiKey: str):
         last_hour_energy = response.json(
         )['energyDetails']['meters'][0]['values'][0].get('value', 0)
 
+    # convert to float
+    last_hour_energy = float(last_hour_energy)
     # return energy info and timestamp
     return last_hour_energy, hour_ago_timestamp
 
@@ -87,11 +133,11 @@ async def main():
     bucket = config.get('INFLUX_BUCKET')
     influx_url = config.get("INFLUX_URL")
 
-    last_hour_energy, hour_ago_timestamp = await get_last_hour_energy(siteId=siteId, apiKey=apiKey)
+    json_body = []
 
-    # open connection to influx
-    with InfluxDBClient(url=influx_url, token=token, org=org) as client:
-        json_body = [
+    if args.hour:
+        last_hour_energy, hour_ago_timestamp = await get_last_hour_energy(siteId=siteId, apiKey=apiKey)
+        json_body.append(
             {
                 "measurement": "energyProducedLastHour",
                 # convert to utc
@@ -100,7 +146,22 @@ async def main():
                     "energy": last_hour_energy,
                 },
             }
-        ]
+        )
+    if args.day:
+        last_day_energy, day_ago_timestamp = await get_last_day_energy(siteId=siteId, apiKey=apiKey)
+        json_body.append(
+            {
+                "measurement": "energyProducedLastDay",
+                # convert to utc
+                "time": day_ago_timestamp.astimezone(pytz.UTC).isoformat(),
+                "fields": {
+                    "energy": last_day_energy,
+                },
+            }
+        )
+
+    # open connection to influx
+    with InfluxDBClient(url=influx_url, token=token, org=org) as client:
         logger.debug(f"Metric Info {json_body}")
         # write data to influxdb
         logger.debug("Attempting to Write to InfluxDB")
@@ -112,8 +173,9 @@ async def main():
 
 if __name__ == "__main__":
     logger = setup_logger()
-
     logger.info("Loading Config")
+
+    args = parse_args()
     config = dotenv_values(".env")
 
     logger.info("Running Main App")
