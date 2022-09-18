07b79ae2dfd2d5a4c78ee7b8f7c19db86d7d96b3
Lee T <lee.t.truong@gmail.com>
2022-09-17 21:03:10 -0500

    reporting to influxdb

--- solar_calc.py
+++ solar_calc.py
@@ -2,17 +2,23 @@ import requests
 import asyncio
 from datetime import datetime, timedelta
 from dotenv import dotenv_values
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+import pytz
+
 config = dotenv_values(".env")
+
 
 async def get_power_details(siteId: int, apiKey: str, startTime: str, endTime: str):
     ''''''
     url = f"https://monitoringapi.solaredge.com/site/{siteId}/powerDetails?api_key={apiKey}&startTime={startTime}&endTime={endTime}&meters=PRODUCTION"
     headers = {
-    'Accept': 'application/json'
+        'Accept': 'application/json'
     }
     response = requests.request("GET", url, headers=headers)
 
     return response
+
 
 async def get_energy_details(siteId: int, apiKey: str, startTime: str, endTime: str, timeUnit: str):
     ''''''
@@ -31,25 +37,47 @@ async def get_last_hour_energy(siteId: int, apiKey: str):
 
     # get current time stamp, rounded to hour
     current_timestamp = datetime.now()
-    current_timestamp = current_timestamp.replace(second=0, minute=0, microsecond=0)
+    current_timestamp = current_timestamp.replace(
+        second=0, minute=0, microsecond=0)
 
     # subtract timestamp by 1 hour
     hour_ago_timestamp = current_timestamp - timedelta(hours=1)
 
     # get energy details
-    response = await get_energy_details(siteId=siteId, apiKey=apiKey, startTime=str(hour_ago_timestamp),endTime=str(current_timestamp), timeUnit="HOUR")
+    response = await get_energy_details(siteId=siteId, apiKey=apiKey, startTime=str(hour_ago_timestamp), endTime=str(current_timestamp), timeUnit="HOUR")
     last_hour_energy = 0
     if response.status_code == 200:
-        last_hour_energy = response.json()['energyDetails']['meters'][0]['values'][0].get('value')
-    return last_hour_energy
+        last_hour_energy = response.json(
+        )['energyDetails']['meters'][0]['values'][0].get('value')
+    return last_hour_energy, hour_ago_timestamp
 
 
 async def main():
     ''''''
     apiKey = config.get('APIKEY')
     siteId = int(config.get('SITEID'))
-    last_hour_energy = await get_last_hour_energy(siteId=siteId, apiKey=apiKey)
 
+    last_hour_energy, hour_ago_timestamp = await get_last_hour_energy(siteId=siteId, apiKey=apiKey)
+
+    # You can generate an API token from the "API Tokens Tab" in the UI
+    token = config.get('INFLUX_TOKEN')
+    org = config.get('INFLUX_ORG')
+    bucket = config.get('INFLUX_BUCKET')
+    influx_url = config.get("INFLUX_URL")
+
+    with InfluxDBClient(url=influx_url, token=token, org=org) as client:
+        json_body = [
+            {
+                "measurement": "energyProduced",
+                # convert to utc
+                "time": hour_ago_timestamp.astimezone(pytz.UTC).isoformat(),
+                "fields": {
+                    "energy": last_hour_energy,
+                },
+            }
+        ]
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+        write_api.write(bucket, org, json_body)
 
 if __name__ == "__main__":
     asyncio.run(main())
