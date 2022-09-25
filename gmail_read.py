7574bd3135a18475cfbbed9abee54cf2dd31064c
Lee T <lee.t.truong@gmail.com>
2022-09-24 19:29:34 -0500

    env var for pickle

--- gmail_read.py
+++ gmail_read.py
@@ -28,8 +28,9 @@ def gmail_authenticate():
     creds = None
     # the file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first time
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
+    pickle_file = os.getenv('TKN_PICKLE', 'token.picke')
+    if os.path.exists(pickle_file):
+        with open(pickle_file, "rb") as token:
             creds = pickle.load(token)
     # if there are no (valid) credentials availablle, let the user log in.
     if not creds or not creds.valid:
@@ -40,7 +41,7 @@ def gmail_authenticate():
                os.getenv('CRD_JSON', 'credentials.json'), SCOPES)
             creds = flow.run_local_server(port=0)
         # save the credentials for the next run
-        with open("token.pickle", "wb") as token:
+        with open(pickle_file, "wb") as token:
             pickle.dump(creds, token)
     return build('gmail', 'v1', credentials=creds)
 
